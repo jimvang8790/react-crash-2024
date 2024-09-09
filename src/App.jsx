2dbaed151b6059bc05338284fcfa3ffb55169e96
jimvang8790 <vangjim15@gmail.com>
2024-09-08 23:19:02 -0500

    add layouts to app mainpage and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
@@ -8,7 +9,11 @@ import JobListings from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
 
 const router = createBrowserRouter (
-  createRoutesFromElements(<Route index element={<HomePage/>} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage/>} />
+    </Route>
+  )
 )
 
 const App = () => {
