37a5169f2e84b43ffe15a537190cf9a9567fa1b6
jimvang8790 <vangjim15@gmail.com>
2024-09-08 23:11:26 -0500

    add routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
 
+const router = createBrowserRouter (
+  createRoutesFromElements(<Route index element={<HomePage/>} />)
+)
+
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
