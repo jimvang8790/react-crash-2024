bda09421c6da5b04ed2792c6351e0d38de301c81
jimvang8790 <vangjim15@gmail.com>
2024-09-08 23:25:46 -0500

    add job page to application

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
-import Navbar from './components/navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter (
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage/>} />
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 )
