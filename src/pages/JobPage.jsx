a47d84a1386b4d23fd250210716c62dfe230903d
jimvang8790 <vangjim15@gmail.com>
2024-09-09 23:38:10 -0500

    swithc from use params to data loader for the sinlge job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLoaderData } from 'react-router-dom';
 import Spinners from '../components/Spinners';
 
 const JobPage = () => {
     const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data);
-            } catch (error) {
-                console.log('Error fetching data from job page', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchJob();
-    }, [])
+    const job = useLoaderData();
+    // useParams
+    // const [job, setJob] = useState(null);
+    // const [loading, setLoading] = useState(true);
+    // 
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`);
+    //             const data = await res.json();
+    //             setJob(data);
+    //         } catch (error) {
+    //             console.log('Error fetching data from job page', error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     }
+    //     fetchJob();
+    // }, []);
     return (
-        loading ? <Spinners /> : (
+        // loading ? <Spinners /> : (
+        //     <h1>{job.title}</h1>
+        // )
             <h1>{job.title}</h1>
-        )
     )
 }
 
-export default JobPage
+// Data Loaders to get a job by it's ID
+// in app.jsx, can import jobLoader
+// much more simple and cleaner and can be used every where else
+// don't have use useState and useEffect for pages that want to use the job json
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+}
+
+// export default JobPage;
+export {JobPage as default, jobLoader};
