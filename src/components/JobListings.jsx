b963582930dfbdbbc03c4f76d0feead46557beb5
jimvang8790 <vangjim15@gmail.com>
2024-09-09 19:54:33 -0500

    adding use state and use effect to job listings page with job json

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import jobs from '../jobs.json';
 import JobListing from './JobListing';
+import { useState, useEffect } from 'react';
 
 const JobListings = ({isHome = false}) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, []);
+
   return (
     <>
         {/* <!-- Browse Jobs --> */}
@@ -13,7 +32,7 @@ const JobListings = ({isHome = false}) => {
             { isHome ? 'Recent Jobs' : 'Browse Jobs'}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobListings.map((job) => (
+            {jobs.map((job) => (
               <JobListing key={job.id} job={job}/>
             ))}
           </div>
