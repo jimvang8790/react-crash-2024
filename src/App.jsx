643f458d0dd20db5c7d09a7d49ced8f7ebe01423
jimvang8790 <vangjim15@gmail.com>
2024-09-10 23:16:37 -0500

    POST request to add job

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,15 @@ import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
-  const addJob = (newJob) => {
-    console.log('new Job', newJob);
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
   }
   
   const router = createBrowserRouter (
