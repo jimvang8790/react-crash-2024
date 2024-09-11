f78d080a410c96e2f0105939f365d1fa95a5e7c7
jimvang8790 <vangjim15@gmail.com>
2024-09-10 23:02:53 -0500

    adding form submission for adding job

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -3,14 +3,32 @@ import { useState } from 'react';
 
 const AddJobPage = () => {
     const [title, setTitle] = useState('');
-    const [type, setType] = useState('');
+    const [type, setType] = useState('Full-Time');
     const [location, setLocation] = useState('');
     const [description, setDescription] = useState('');
-    const [salary, setSalary] = useState('');
+    const [salary, setSalary] = useState('Under $50K');
     const [companyName, setCompanyName] = useState('');
     const [companyDescription, setCompanyDescription] = useState('');
     const [contactEmail, setContactEmail] = useState('');
     const [contactPhone, setContactPhone] = useState('');
+
+    const sumbitForm = (e) => {
+        e.preventDefault();
+        const newJob = {
+            title,
+            type,
+            location,
+            description,
+            salary,
+            company: {
+                name: companyName,
+                description: companyDescription,
+                contactEmail: contactEmail,
+                contactPhone: contactPhone
+            }
+        }
+        console.log('newJob', newJob);
+    }
 
     return (
         <>
@@ -19,7 +37,7 @@ const AddJobPage = () => {
                     <div
                     className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
                     >
-                    <form>
+                    <form onSubmit={(sumbitForm)}>
                         <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
                         <div className="mb-4">
