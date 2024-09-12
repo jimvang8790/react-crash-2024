d03abc98dabedd89f214864c702a0c17a680aa89
jimvang8790 <vangjim15@gmail.com>
2024-09-12 00:06:09 -0500

    add function for edit job update form submission and PUT request

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   //Add New Job
@@ -27,6 +28,18 @@ const App = () => {
     });
     return;
   }
+
+  //Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
   
   const router = createBrowserRouter (
     createRoutesFromElements(
@@ -34,7 +47,16 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
-        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+        <Route 
+          path='/edit-job/:id' 
+          element={<EditJobPage updateJobSubmit={updateJob}/>}
+          loader={jobLoader}
+        />
+        <Route 
+          path='/jobs/:id' 
+          element={<JobPage deleteJob={deleteJob}/>} 
+          loader={jobLoader}
+        />
         <Route path='*' element={<NotFoundPage />}/>
       </Route>
     )
